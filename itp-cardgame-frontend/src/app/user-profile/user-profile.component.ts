6cf8e8f6ec72e9c61c109fc088f1fa382496c875
Aaron Domingo <daaronjhay@gmx.at>
2025-04-13 10:39:07 +0200

    created new page

--- itp-cardgame-frontend/src/app/user-profile/user-profile.component.ts
+++ itp-cardgame-frontend/src/app/user-profile/user-profile.component.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucidePen, lucideTrophy } from '@ng-icons/lucide';
+import { HlmSeparatorDirective } from '@spartan-ng/ui-separator-helm';
+import { BrnSeparatorComponent } from '@spartan-ng/brain/separator';
+import {
+  HlmTableComponent,
+  HlmTdComponent,
+  HlmThComponent,
+  HlmTrowComponent,
+} from '@spartan-ng/ui-table-helm';
+
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  providers: [provideIcons({ lucidePen, lucideTrophy })],
+  imports: [
+    HlmButtonDirective,
+    HlmIconDirective,
+    NgIcon,
+    HlmSeparatorDirective,
+    BrnSeparatorComponent,
+    HlmTableComponent,
+    HlmTdComponent,
+    HlmThComponent,
+    HlmTrowComponent,
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
