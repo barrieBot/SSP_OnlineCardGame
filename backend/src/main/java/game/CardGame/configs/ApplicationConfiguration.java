8f72cc82eff9bdaccfc3c421a3d3aaea54a369d8
FabioHa-Ha <if24b506@technikum-wien.at>
2025-06-11 12:02:21 +0200

    Adds CORS Config for localhost:4200

--- backend/src/main/java/game/CardGame/configs/ApplicationConfiguration.java
+++ backend/src/main/java/game/CardGame/configs/ApplicationConfiguration.java
@@ -3,6 +3,8 @@ package game.CardGame.configs;
 import game.CardGame.repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +12,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -43,5 +47,20 @@ public class ApplicationConfiguration {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return  new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods(HttpMethod.GET.name(),
+                                        HttpMethod.POST.name())
+                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+                                        HttpHeaders.AUTHORIZATION);
+            }
+        };
     }
 }
